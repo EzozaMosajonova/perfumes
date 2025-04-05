b2279b21e695d39b35124625544a219bd45e074d
Fesleen <mosajonovae@gmail.com>
2025-04-05 14:33:37 +0500

    cjbd

--- src/Components/Contact/index.jsx
+++ src/Components/Contact/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import axios from "axios";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-
-const ContactUs = () => {
+const Contact = () => {
   const sendMessage = (event) => {
     event.preventDefault();
 
@@ -32,113 +32,117 @@ const ContactUs = () => {
  Xabar: ${message}
 `;
 
-    axios.post(url, {
-      chat_id: chat_id,
-      text: messageContent,
-    })
+    axios
+      .post(url, {
+        chat_id: chat_id,
+        text: messageContent,
+      })
       .then(() => {
-        alert("Xabar muvaffaqiyatli yuborildi!");
+        toast.success("Xabar muvaffaqiyatli yuborildi!");
         document.getElementById("myform").reset();
       })
       .catch((error) => {
         console.error("Xabar yuborishda xatolik:", error);
+        toast.error("Xatolik yuz berdi. Iltimos, keyinroq qayta urinib ko‘ring.");
       });
   };
 
   return (
-   <div id="contact" className="bg-amber-50 py-10 mt-10">
-     <div className="container max-w-[1260px] mx-auto px-4 ">
-      <h2 className="text-black md:font-extrabold font-bold md:text-5xl text-3xl text-center">
-        Contact Us
-      </h2>
-      <div className=" flex items-center justify-center mt-20">
-        <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10">
-          {/* RIGHT SIDE - Contact Info */}
-          <div className="order-1 md:order-2 flex flex-col justify-center">
+    <div id="contact" className="bg-amber-50 py-10 mt-10">
+      <div className="container max-w-[1260px] mx-auto px-4 ">
+        <h2 className="text-black md:font-extrabold font-bold md:text-5xl text-3xl text-center">
+          Contact Us
+        </h2>
+        <div className="flex items-center justify-center mt-20">
+          <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-10">
+            {/* RIGHT SIDE - Contact Info */}
+            <div className="order-1 md:order-2 flex flex-col justify-center">
               <p className="mb-4">
-                <span className="font-bold text-2xl">Email:</span>{" "}info@luxlineuae.com
+                <span className="font-bold text-2xl">Email:</span>{" "}
+                info@luxlineuae.com
               </p>
-            <p className="mb-4">
-              <span className="font-bold text-2xl">Phone:</span> +971 52 133
-              0070
-            </p>
-            <p>
-              <span className="font-bold text-2xl">Address:</span> 11 Shaikh
-              Mohammed Bin Zayed street – Al Sajaa – Sharjah
-            </p>
-          </div>
-
-          {/* LEFT SIDE - Form */}
-          <form
-            id="myform"
-            onSubmit={sendMessage}
-            className="order-2 md:order-1 space-y-4"
-          >
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              <input
-                id="name"
-                type="text"
-                placeholder="Name*"
-                pattern="^[A-Za-z\s]+$"
-                title="Ism faqat harflardan iborat bo‘lishi kerak"
-                className="border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
-                required
-              />
-
-              <input
-                id="number"
-                type="tel"
-                placeholder="Phone Number*"
-                pattern="^\+998\d{9}$"
-                title="Telefon raqam +998 bilan boshlanib, jami 9 ta raqamdan iborat bo‘lishi kerak"
-                className="border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
-                required
-                defaultValue="+998"
-                onFocus={(e) => {
-                  if (!e.target.value.startsWith("+998")) {
-                    e.target.value = "+998";
-                  }
-                }}
-                onChange={(e) => {
-                  const cleaned = e.target.value.replace(/[^\d]/g, "");
-                  e.target.value = "+998" + cleaned.slice(3, 12);
-                }}
-              />
-
-
+              <p className="mb-4">
+                <span className="font-bold text-2xl">Phone:</span> +971 52 133
+                0070
+              </p>
+              <p>
+                <span className="font-bold text-2xl">Address:</span> 11 Shaikh
+                Mohammed Bin Zayed street – Al Sajaa – Sharjah
+              </p>
             </div>
-            <input
-              id="email"
-              type="email"
-              placeholder="Email*"
-              className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
-              required
-            />
-            <input
-              id="subject"
-              type="text"
-              placeholder="Your subject"
-              className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
-            />
-            <textarea
-              id="message"
-              placeholder="Message"
-              rows="6"
-              className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
-              required
-            ></textarea>
-            <button
-              type="submit"
-              className="bg-[#fff5ef] text-black font-bold py-3 px-10 rounded-lg border border-black hover:bg-black hover:text-white transition-all"
+
+            {/* LEFT SIDE - Form */}
+            <form
+              id="myform"
+              onSubmit={sendMessage}
+              className="order-2 md:order-1 space-y-4"
             >
-              SEND
-            </button>
-          </form>
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <input
+                  id="name"
+                  type="text"
+                  placeholder="Name*"
+                  pattern="^[A-Za-z\s]+$"
+                  title="Ism faqat harflardan iborat bo‘lishi kerak"
+                  className="border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
+                  required
+                />
+
+                <input
+                  id="number"
+                  type="tel"
+                  placeholder="Phone Number*"
+                  pattern="^\+998\d{9}$"
+                  title="Telefon raqam +998 bilan boshlanib, jami 9 ta raqamdan iborat bo‘lishi kerak"
+                  className="border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
+                  required
+                  defaultValue="+998"
+                  onFocus={(e) => {
+                    if (!e.target.value.startsWith("+998")) {
+                      e.target.value = "+998";
+                    }
+                  }}
+                  onChange={(e) => {
+                    const cleaned = e.target.value.replace(/[^\d]/g, "");
+                    e.target.value = "+998" + cleaned.slice(3, 12);
+                  }}
+                />
+              </div>
+              <input
+                id="email"
+                type="email"
+                placeholder="Email*"
+                className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
+                required
+              />
+              <input
+                id="subject"
+                type="text"
+                placeholder="Your subject"
+                className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
+              />
+              <textarea
+                id="message"
+                placeholder="Message"
+                rows="6"
+                className="w-full border border-red-200 rounded-lg px-4 py-3 focus:outline-none"
+                required
+              ></textarea>
+              <button
+                type="submit"
+                className="bg-[#fff5ef] text-black font-bold py-3 px-10 rounded-lg border border-black hover:bg-black hover:text-white transition-all"
+              >
+                SEND
+              </button>
+            </form>
+          </div>
         </div>
       </div>
+
+      {/* ToastContainerni shu yerda qo‘shamiz */}
+      <ToastContainer position="top-right" autoClose={3000} />
     </div>
-   </div>
   );
 };
 
-export default ContactUs;
+export default Contact;
